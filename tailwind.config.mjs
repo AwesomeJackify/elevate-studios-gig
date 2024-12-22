b440011f015c17cb41d121231677c4e799c9906d
Jack Gong <jackgong48@gmail.com>
2024-12-23 11:00:23 +1300

    ADDED: font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Kabel", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   daisyui: {
     themes: [
