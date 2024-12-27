821d80ddd58f0d2091940877cde5a2be1d5c75ca
Jack Gong <jackgong48@gmail.com>
2024-12-27 13:39:23 +1300

    ADDED: programs and services collections

---
+++ src/data/programs.ts
@@ -0,0 +1,30 @@
+
+import outsoleLogo from "../assets/images/outsole.png";
+import kumeteLogo from "../assets/images/kumete.png";
+import elevateLogo from "../assets/images/elevate.png";
+
+const programs = [
+  {
+    name: "Outsole",
+    description:
+      "Empowering local artists and creators through workshops and mentorship opportunities.",
+    image: outsoleLogo,
+    url: "/programs/outsole",
+  },
+  {
+    name: "Kumete",
+    description:
+      "Supporting sustainable practices in our community through educational initiatives and collaborative projects.",
+    image: kumeteLogo,
+    url: "/programs/kumete",
+  },
+  {
+    name: "Elevate",
+    description:
+      "Providing resources and platforms for underrepresented voices, ensuring everyone has the opportunity to share their story.",
+    image: elevateLogo,
+    url: "/programs/elevate",
+  },
+];
+
+export default programs;
