0df1eeac2b542fa4c9cba96316d109e9a7067a19
Jack Gong <jackgong48@gmail.com>
2025-03-05 19:16:51 +1300

    UPDATED: the site to be SSR mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
+  adapter: netlify(),
 });
