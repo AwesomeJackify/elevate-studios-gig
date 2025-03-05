43cbc531eab421fc98b45fda8a13eb02130e77a8
Jack Gong <jackgong48@gmail.com>
2025-03-05 19:51:44 +1300

    ADDED: new content

--- src/data/services.ts
+++ src/data/services.ts
@@ -31,7 +31,7 @@ const services = [
       "At the heart of every great brand is compelling content. Our content creation services are designed to tell your story, captivate your audience, and drive results. From engaging videos to eye-catching graphics and written content, we craft customized material that aligns with your brand voice and goals. \n\nOur team of creative professionals works closely with you to develop content that resonates with your target audience. Whether it’s social media posts, blog articles, or promotional videos, we ensure your content not only grabs attention but also sparks meaningful engagement. Let us help you build a strong, authentic presence that elevates your brand and grows your business.",
     icon: "mdi:monitor-cellphone-star",
     url: "/services",
-    videoUrl: cld.video("services/content").toURL(),
+    videoUrl: cld.video("services/content2").toURL(),
   },
 ];
 
